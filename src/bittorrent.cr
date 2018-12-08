323d0a8c8fbbfaa7b553e9141918b4e5f324fa15
Yannick Utard <yannickutard@gmail.com>
2018-12-08 21:45:07 +0100

    tracker: GET /announce

--- src/bittorrent.cr
+++ src/bittorrent.cr
@@ -1,4 +1,6 @@
-require "digest"
+require "http"
+require "openssl"
+require "socket"
 require "uri"
 
 module BitTorrent
@@ -8,4 +10,17 @@ end
 require "./bittorrent/bencoding/*"
 require "./bittorrent/*"
 
-BitTorrent::Tracker.new("test.torrent")
+def handle_client(client)
+  message = client.gets
+  client.puts message
+end
+
+spawn do
+  server = TCPServer.new("localhost", 6881)
+  while client = server.accept?
+    spawn handle_client(client)
+  end
+end
+
+tracker = BitTorrent::Tracker.new("test.torrent")
+tracker.register
--- src/bittorrent/tracker.cr
+++ src/bittorrent/tracker.cr
@@ -1,27 +1,54 @@
 module BitTorrent
   class Tracker
     def initialize(torrent_file)
-      metadata = BEncoding.decode(File.read(torrent_file)).as(Hash(String, BEncoding::Node))
-      puts typeof(metadata)
-      puts metadata
-      infos = metadata["info"].as(Hash(String, BEncoding::Node))
-      encode = BEncoding.encode(infos)
-      puts encode
-      puts Digest::SHA1.digest(encode)
-      puts build_url(metadata["announce"].as(String), "-MB2020-", infos["length"])
+      @metadata = BEncoding.decode(
+        File.read(torrent_file)
+      ).as(Hash(String, BEncoding::Node))
     end
 
     def register
+      infos = @metadata["info"].as(Hash(String, BEncoding::Node))
+      info_hash = OpenSSL::SHA1.hash(BEncoding.encode(infos)).to_slice
+
+      resp = HTTP::Client.get(
+        build_url(
+          @metadata["announce"].as(String),
+          String.new(info_hash),
+          infos["length"]
+        )
+      )
+
+      body = BEncoding.decode(resp.body)
+
+      peers = body.as(Hash(String, BEncoding::Node))["peers"].as(String)
+
+      peers.bytes.in_groups_of(6, filled_up_with = 0.to_u8).map do |peer|
+        case peer
+        when Array
+          ip = Slice.new(peer[0, 4].to_unsafe, 4)
+          port = Slice.new(peer[4, 2].to_unsafe, 2)
+          puts ip.map(&.to_i).join('.')
+          puts port
+
+          i = IO::ByteFormat::NetworkEndian.decode(UInt32, ip)
+          p = IO::ByteFormat::NetworkEndian.decode(UInt16, port)
+
+          {i, p}
+        else
+          raise "Peer wasn't an array"
+        end
+      end
     end
 
     def peers
     end
 
-    private def build_url(endpoint : String, peer_id, length)
+    private def build_url(endpoint, info_hash, length)
       url = URI.parse(endpoint)
+
       query = {
-        "info_hash"  => "f%60%21%95%EE%24%B6%15M%CB%EBLP%C7%B0%D11%5C%868",
-        "peer_id"    => peer_id,
+        "info_hash"  => URI.escape(info_hash),
+        "peer_id"    => "-AZ2060-xxxxxxxxxxxx",
         "port"       => "6881",
         "compact"    => "1",
         "downloaded" => "0",
