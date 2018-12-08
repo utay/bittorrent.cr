ce393907ada3f78c93af2b61b2489443fdf9e130
Yannick Utard <yannickutard@gmail.com>
2018-12-08 23:03:49 +0100

    tracker: refactoring .torrent parsing

---
+++ src/bittorrent/torrent.cr
@@ -0,0 +1,33 @@
+module BitTorrent
+  class Torrent
+    def initialize(torrent_file)
+      @metadata = BEncoding.decode(
+        File.read(torrent_file)
+      ).as(Hash(String, BEncoding::Node))
+    end
+
+    def infos
+      @metadata["info"].as(Hash(String, BEncoding::Node))
+    end
+
+    def announce
+      @metadata["announce"].as(String)
+    end
+
+    def info_hash
+      OpenSSL::SHA1.hash(BEncoding.encode(self.infos))
+    end
+
+    def name
+      @metadata["name"].as(String)
+    end
+
+    def length
+      self.infos["length"].as(Int64)
+    end
+
+    def pieces
+      self.infos["pieces"].as(String)
+    end
+  end
+end
